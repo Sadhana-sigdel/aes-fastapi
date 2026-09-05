10920cee07a5cf159c828edd4f8081b1044f15b3
Sadhana-sigdel <s.sadhana.0323@gmail.com>
2026-09-05 23:56:28 +0545

    feat: add encryption endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+class EncryptRequest(BaseModel):
+	plaintext: str
+	key: str
 app = FastAPI()
 @app.get("/")
 def home():
 	return {"message": "Hello, World!"}
 @app.post("/encrypt")
-def encrypt():
+def encrypt(request: EncryptRequest):
+	print(request.plaintext)
+	print(request.key)
 	return {"message": "Encrypt"}
 # @app.post("/decrypt")
 # def decrypt():
