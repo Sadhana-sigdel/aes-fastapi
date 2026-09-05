ee785c36f6448b3ac0dd768c9ca025455d27259a
Sadhana-sigdel <s.sadhana.0323@gmail.com>
2026-09-05 21:22:53 +0545

    feat: add encryption endpoint

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,10 @@ app = FastAPI()
 @app.get("/")
 def home():
 	return {"message": "Hello, World!"}
+@app.post("/encrypt")
+def encrypt():
+	return {"message": "Encrypt"}
+# @app.post("/decrypt")
+# def decrypt():
+# 	return {"message": "Decrypt"}
     
